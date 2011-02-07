1d76aa53861d0fe3248f8227e31e1c7a1d34bd14
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-02-07 15:00:09 +0100

    Fixed stopwords claim

--- lib/rbbt/bow/misc.rb
+++ lib/rbbt/bow/misc.rb
@@ -1,7 +1,7 @@
 require 'rbbt'
 require 'rbbt/util/open'
 
-Rbbt.claim 'stopwords', nil, 'wordlist'
+Rbbt.claim 'stopwords', nil, 'wordlists'
 
 $stopwords = Rbbt.files.wordlists.stopwords.read.scan(/\w+/)
 
