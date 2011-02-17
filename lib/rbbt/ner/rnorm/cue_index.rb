8f7b322552d070e05fb73709c61deaf58fbf801a
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-02-17 17:33:57 +0100

    Added support for normalization

---
+++ lib/rbbt/ner/rnorm/cue_index.rb
@@ -0,0 +1,82 @@
+require 'rbbt-util'
+require 'rbbt/util/misc'
+require 'rbbt/util/simpleDSL'
+
+class CueIndex
+  include SimpleDSL
+
+  class LexiconMissingError < StandardError; end
+
+
+  def define(name, *args, &block)
+    @rules << [name,block]
+    nil
+  end
+
+  def initialize(file = nil, &block)
+    @rules   = []
+
+    file ||= Rbbt.files.rnorm.cue_default.produce if !file && !block
+
+    load_config(:define, file, &block)
+  end
+
+  def config
+    @config[:define]
+  end
+
+
+  def cues(word)
+    @rules.collect{|rule|
+      c = rule[1].call(word)
+      c = [c] unless  c.is_a? Array 
+      c
+    }
+  end
+
+  def clean(max)
+    @indexes.each{|index|
+      remove = []
+      index.each{|key,values|
+        remove << key if values.length > max
+      }
+      remove.each{|key|
+        index.delete(key)
+      }
+    }
+  end
+  
+  def load(file, max_candidates = 50)
+    @indexes = Array.new(@rules.size){Hash.new}
+    data = TSV.new(file)
+    data.each{|code, values_lists|
+      values = values_lists.flatten.compact.uniq
+      values.each{|value|
+        cues(value).each_with_index{|cue_list,i|
+          cue_list.each{|cue|
+            @indexes[i][cue] ||= []
+            @indexes[i][cue]  << code unless @indexes[i][cue].include? code
+          }
+        }
+      }
+    }
+    clean(max_candidates) if max_candidates
+    nil
+  end
+
+  def match(name)
+    raise LexiconMissingError, "Load Lexicon before matching" unless @indexes
+
+    cues = cues(name)
+    @indexes.each_with_index{|index,i|
+      best = []
+      cues[i].each{|cue|
+        best << index[cue] if index[cue]
+      }
+      return best.flatten if best.any?
+    }
+
+    return []
+  end
+
+end
