6c40225d1bf61996eac33a7144bb527fe141cbe2
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-05-03 12:44:12 +0200

    Added OSCAR4

---
+++ lib/rbbt/ner/oscar4.rb
@@ -0,0 +1,38 @@
+require 'rbbt'
+require 'rjb'
+require 'libxml'
+require 'rbbt/ner/annotations'
+require 'rbbt/ner/NER'
+require 'rbbt/util/log'
+
+class OSCAR4 < NER
+  Rbbt.software.opt.OSCAR4.define_as_install Rbbt.share.install.software.OSCAR4.find
+
+  @@OSCAR = Rjb::import('uk.ac.cam.ch.wwmm.oscar.Oscar')
+
+  def self.match(text,  type = nil, memm =  false)
+
+    oscar = @@OSCAR.new();
+    entities = oscar.findAndResolveNamedEntities(text);
+    it = entities.iterator
+
+    result = []
+
+    while it.hasNext
+      entity = it.next
+      mention = entity.getSurface
+      result << mention
+
+      NamedEntity.annotate mention, entity.getStart, entity.getType, nil, entity.getNamedEntity.getConfidence
+    end
+
+    result
+  end
+
+  def match(*args)
+    OSCAR4.match *args
+  end
+end
+
+
+
---
+++ test/rbbt/ner/test_oscar4.rb
@@ -0,0 +1,57 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), '../../test_helper.rb')
+require 'rbbt/ner/oscar4'
+require 'rbbt/util/tmpfile'
+require 'test/unit'
+
+class TestOSCAR4 < Test::Unit::TestCase
+
+  def test_match
+    begin
+      ner = OSCAR4.new
+      str  = "Alternatively, rearrangement of O-(w-haloalkyl)esters 34 of 2-carboethoxy-N-hydroxypyridine-2-selone affords azonianaphthalenium halides 37 in 79% yield"
+
+      mentions = ner.match(str, "CM", false)
+      good_mentions = ["2-carboethoxy-N-hydroxypyridine-2-selone", "O-(w-haloalkyl)esters"]
+
+      good_mentions.each{|mention| 
+        assert(mentions.include? mention)
+      }
+    rescue
+      puts $!.message
+      puts $!.backtrace
+    end
+  end
+
+  def test_ranges
+    begin
+      ner = OSCAR4.new
+      str =<<-EOF 
+This sentence talks about 2-carboethoxy-N-hydroxypyridine-2-selone.
+This sentence talks about 2-carboethoxy-N-hydroxypyridine-2-selone.
+This sentence talks about 2-carboethoxy-N-hydroxypyridine-2-selone.
+This sentence talks about 2-carboethoxy-N-hydroxypyridine-2-selone.
+This otherone talks about O-(w-haloalkyl)esters.
+This otherone talks about O-(w-haloalkyl)esters.
+This otherone talks about O-(w-haloalkyl)esters.
+
+This otherone talks about O-(w-haloalkyl)esters.
+This otherone talks about O-(w-haloalkyl)esters.
+      EOF
+
+
+      mentions = ner.match(str, "CM", false)
+
+      str_original = str.dup
+      mentions.each do |mention|
+        str[mention.range] = mention
+      end
+
+      assert_equal str_original, str
+
+    rescue
+      puts $!.message
+      puts $!.backtrace
+    end
+  end
+
+end
