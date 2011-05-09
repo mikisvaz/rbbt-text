5dbe8e564d1d39e0f75a662f5950a5027ec67794
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-05-09 12:16:37 +0200

    nil

--- lib/rbbt/ner/oscar4.rb
+++ lib/rbbt/ner/oscar4.rb
@@ -8,6 +8,7 @@ require 'rbbt/util/log'
 class OSCAR4 < NER
   Rbbt.software.opt.OSCAR4.define_as_install Rbbt.share.install.software.OSCAR4.find
 
+  Rjb::load(nil, jvmargs = ['-Xms128m','-Xmx2048m'])
   @@OSCAR = Rjb::import('uk.ac.cam.ch.wwmm.oscar.Oscar')
 
   def self.match(text,  type = nil, memm =  false)
