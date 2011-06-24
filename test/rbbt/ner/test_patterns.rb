f11abc1a5342149eeb49ce0168c98779da44491f
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-06-24 12:05:47 +0200

    Patterns

---
+++ test/rbbt/ner/test_patterns.rb
@@ -0,0 +1,66 @@
+require File.join(File.expand_path(File.dirname(__FILE__)), '../../test_helper.rb')
+require 'rbbt/ner/patterns'
+
+class TestPatternRelExt < Test::Unit::TestCase
+  def test_simple_pattern
+    text = "Experiments have shown that TP53 interacts with CDK5 under certain conditions"
+
+    gene1 = "TP53"
+    NamedEntity.annotate(gene1, text.index(gene1), "Gene")
+
+    gene2 = "CDK5"
+    NamedEntity.annotate(gene2, text.index(gene2), "Gene")
+
+    interaction = "interacts"
+    NamedEntity.annotate(interaction, text.index(interaction), "Interaction")
+
+    Annotated.annotate(text, [gene1, gene2, interaction])
+
+    assert_equal "TP53 interacts with CDK5", PatternRelExt.simple_pattern(text, "GENE INTERACTION with GENE").first
+
+  end
+
+  def test_chunk_pattern
+    text = "Experiments have shown that TP53 found in cultivated cells interacts with CDK5 under certain conditions"
+
+    gene1 = "TP53"
+    NamedEntity.annotate(gene1, text.index(gene1), "Gene")
+
+    gene2 = "CDK5"
+    NamedEntity.annotate(gene2, text.index(gene2), "Gene")
+
+    interaction = "interacts"
+    NamedEntity.annotate(interaction, text.index(interaction), "Interaction")
+
+    Annotated.annotate(text, [gene1, gene2, interaction])
+
+    assert_equal "TP53 found in cultivated cells interacts with CDK5", 
+      PatternRelExt.new("NP[entity:Gene] VP[stem:interacts] with NP[entity:Gene]").match_sentences([text]).first.first
+
+    assert_equal "TP53 found in cultivated cells interacts with CDK5", 
+      PatternRelExt.new(["NP[entity:Gene] VP[stem:interacts] with NP[entity:Gene]"]).match_sentences([text]).first.first
+  end
+
+  def test_chunk_pattern
+    text = "There is a concern with the use of thiazolidinediones in patients with an increased risk of colon cancer (e.g., familial colon polyposis)."
+
+    drug = "thiazolidinediones"
+    NamedEntity.annotate(drug, text.index(drug), "Chemical Mention")
+
+    disease = "colon cancer"
+    NamedEntity.annotate(disease, text.index(disease), "disease")
+
+    Annotated.annotate(text, [drug, disease])
+
+    assert_equal "thiazolidinediones in patients with an increased risk of colon cancer", 
+      PatternRelExt.new("NP[entity:Chemical Mention] NP[stem:risk] NP[entity:disease]").match_sentences([text]).first.first
+
+  end
+
+
+  def test_entities_with_spaces
+    PatternRelExt.new("NP[entity:Gene Name]").token_trie
+  end
+
+
+end
