0d599211746305d1b683d0a8af6c6d3d0a20ad3a
Miguel Vazquez <miguel.vazquez@cnio.es>
2014-08-26 15:14:57 +0200

    nil

--- test/rbbt/ner/test_finder.rb
+++ test/rbbt/ner/test_finder.rb
@@ -9,13 +9,13 @@ require 'rbbt/sources/NCI'
 class TestFinder < Test::Unit::TestCase
 
   def test_namespace_and_format
-    f = Finder.new(CMD.cmd("head -n 1000", :in => Open.open(Organism.identifiers("Hsa/jun2011").produce.find)))
+    f = Finder.new(CMD.cmd("head -n 1000", :in => Open.open(Organism.identifiers(Organism.default_code("Hsa")).produce.find)))
     assert_equal "Hsa", f.instances.first.namespace
     assert_equal "Ensembl Gene ID", f.instances.first.format
   end
 
   def test_find
-    f = Finder.new(Organism.lexicon("Hsa/jun2011"), :grep => ["SF3B1"])
+    f = Finder.new(Organism.lexicon(Organism.default_code("Hsa")), :grep => ["SF3B1"])
 
     assert_equal "ENSG00000115524", f.find("SF3B1").first
     if defined? Entity
@@ -25,7 +25,7 @@ class TestFinder < Test::Unit::TestCase
   end
 
   def test_find
-    f = Finder.new(Organism.lexicon("Hsa/jun2011"), :grep => ["RASGRF2"])
+    f = Finder.new(Organism.lexicon(Organism.default_code("Hsa")), :grep => ["RASGRF2"])
 
     assert f.find("RAS").include? "ENSG00000113319"
   end
