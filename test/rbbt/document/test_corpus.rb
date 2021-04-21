3336af565c43e62f19b80450c145b6388866cc5f
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2021-04-21 15:37:35 +0200

    nil

--- test/rbbt/document/test_corpus.rb
+++ test/rbbt/document/test_corpus.rb
@@ -29,5 +29,19 @@ class TestDocumentCorpus < Test::Unit::TestCase
       assert corpus.docids("TEST:").include?(text.docid)
     end
   end
+
+  def test_load
+    text = "This is a document"
+    Document.setup(text, "TEST", "test_doc1", nil)
+
+    TmpFile.with_file do |path|
+      corpus = Persist.open_tokyocabinet(path, true, :single, "BDB")
+      corpus.extend Document::Corpus
+
+      corpus.add_document(text)
+
+      assert corpus.docids("TEST:").include?(text.docid)
+    end
+  end
 end
 
